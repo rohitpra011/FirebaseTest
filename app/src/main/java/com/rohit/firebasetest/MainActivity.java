b1d01770d0b9dc06e1186419195ec39693a95c9e
Rohit Prajapati <rp53563@gmail.com>
2022-12-27 14:30:02 +0530

    firebase realtime database implemented. one edittext and one add button also added to save textual data in firebase realtime database. Both of these edittext and button added to the activity which contain logout button.

--- app/src/main/java/com/rohit/firebasetest/MainActivity.java
+++ app/src/main/java/com/rohit/firebasetest/MainActivity.java
@@ -6,13 +6,15 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText text;
+    private Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,StartActivity.class));
             }
         });
-        FirebaseDatabase.getInstance().getReference().child("Rohit Prajapati").setValue("Student");
+      //  FirebaseDatabase.getInstance().getReference().child("Rohit Prajapati").child(" VIT ").setValue("Student");
+    text=findViewById(R.id.TextToBeAdded);
+    add=findViewById(R.id.AddButton);
+    add.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String text_add= text.getText().toString();
+            if(text_add.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please type some text", Toast.LENGTH_SHORT).show();
+                }
+            else{
+                    FirebaseDatabase.getInstance().getReference().child("Rohit Prajapati").push().setValue(text_add);
+                }
+        }
+    });
+
     }
 }
