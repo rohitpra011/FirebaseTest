11c8c5569a8553b46217b1593c59c8b5cc2eec91
Rohit Prajapati <rp53563@gmail.com>
2023-01-14 22:17:45 +0530

    CompileSdk version upgraded to 33

---
+++ app/src/main/java/com/rohit/firebasetest/UserData.java
@@ -0,0 +1,16 @@
+package com.rohit.firebasetest;
+
+public class UserData {
+    public String fullName, age, email;
+
+    public UserData()
+    {
+    }
+
+    public UserData(String fullName, String age, String email)
+    {
+        this.fullName=fullName;
+        this.age=age;
+        this.email=email;
+    }
+}
